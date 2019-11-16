904856ecc25e191b25c1b1f3376efd70bde27395
onur sisoglu <o.sisoglu@gmail.com>
2019-11-16 10:08:39 +0300

    Ders3

---
+++ E_Commerce_NTier/ECommerce.UI.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using ECommerce.Business.Repository;
+using ECommerce.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerce.UI.Web.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddCategory()
+        {
+            // Kullanıcıya sayfayı göstermek için var.
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult AddCategory(Category model)
+        {
+            CategoryRepository katRepo = new CategoryRepository();
+
+            katRepo.Add(model);
+
+            return View();
+        }
+
+
+    }
+}
